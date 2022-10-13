9ffa252940691267b1d42bf2bea9c69f65fd47a4
kashi93 <75873210+kashi93@users.noreply.github.com>
2022-10-13 23:56:01 +0800

    Updated 13/10/2022

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./lib/resources/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "app.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
