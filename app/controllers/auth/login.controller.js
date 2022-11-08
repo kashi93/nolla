826f710f9b5b93242e7146c5298642ed9b19caa7
kashi93 <75873210+kashi93@users.noreply.github.com>
2022-11-09 00:52:01 +0800

    Updated 9/11/2022

--- app/controllers/auth/login.controller.js
+++
@@ -1,8 +0,0 @@
-const Controller = require("../controller");
-const DefaultAuth = require("../../../vendor/rainbows/defaultAuth");
-const { applyMixins } = require("../../../vendor/rainbows/applyMixins");
-
-applyMixins(Controller, [DefaultAuth]);
-
-class LoginController extends Controller {}
-module.exports = LoginController;
--- routes/web.js
+++ routes/web.js
@@ -1,28 +1,5 @@
 var Route = require("../vendor/route/route").default;
-Route.middleware("user_not_login", function () {
-  Route.get("/", function () {
-    return view("welcome", { layout: "nolla/templates/app" });
-  }).name("home");
-  Route.prefix("user", function () {
-    Route.get("/", ["user.controller", "index"]).name("user.index");
-    Route.get("/create", ["user.controller", "create"]).name("user.create");
-    Route.post("/", ["user.controller", "store"]).name("user.store");
-    Route.get("/:id/edit", ["user.controller", "edit"]).name("user.edit");
-    Route.post("/:id/update", ["user.controller", "update"]).name(
-      "user.update"
-    );
-  });
-  Route.get("/log-out", ["auth/login.controller", "logout"]).name("logout");
-});
-Route.middleware("user_is_login", function () {
-  Route.get("/register", ["auth/register.controller", "index"]).name(
-    "register_user"
-  );
-  Route.post("/register", ["auth/register.controller", "create"]).name(
-    "register"
-  );
-  Route.get("/login", ["auth/login.controller", "index"]).name("login");
-  Route.post("/login", ["auth/login.controller", "login"]).name(
-    "login_attempt"
-  );
+
+Route.get("/", function () {
+  return view("welcome");
 });
